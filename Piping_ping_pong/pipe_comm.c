95eb8c92fff89fe07c7f35e1e06a56121e52c130
Sam Tagart Cottis <Homeostatic@protonmail.com>
2025-01-18 21:40:52 +0100

    pingpong done

--- Piping_ping_pong/pipe_comm.c
+++ Piping_ping_pong/pipe_comm.c
@@ -10,6 +10,7 @@ enum {READ = 0, WRITE =1};
 const char *PING = "Ping";
 const char *PONG = "Pong";
 
+
 /**
  * @brief Create twp pipes and call perror and exit, if the creation fails.
  */
@@ -25,10 +26,10 @@ void create_pipes() {
  */
 void tell_parent() {
   /* TODO: Pipe handling missing*/
-  close(pipefd_ptc[WRITE]);
-  close(pipefd_ctp[READ]);
+  //close(pipefd_ptc[WRITE]);
+  //close(pipefd_ctp[READ]);
   write(pipefd_ctp[WRITE],PONG,sizeof(PONG)+1);
-  close(pipefd_ctp[WRITE]);
+  //close(pipefd_ctp[WRITE]);
   printf("Child sent a message to parent!\n");
 
 }
@@ -37,12 +38,11 @@ void tell_parent() {
  * @brief Child reads a message out of the paper that was sent by the parent
  */
 void wait_parent() {
-  /* TODO: Pipe handling missing*/
   char buf[99];  
   ssize_t byte_count;
-  close(pipefd_ptc[WRITE]);
+  //close(pipefd_ptc[WRITE]);
   byte_count = read(pipefd_ptc[READ], buf, sizeof(buf));
-  close(pipefd_ptc[READ]);
+  //close(pipefd_ptc[READ]);
   buf[byte_count] = '\0';
   printf("Child received %s from the parent!\n", buf);
 }
@@ -51,11 +51,10 @@ void wait_parent() {
  * 
  */
 void tell_child() {
-  /* TODO: Pipe handling missing*/
-  close(pipefd_ctp[WRITE]);
-  close(pipefd_ptc[READ]);
+  //close(pipefd_ctp[WRITE]);
+  //close(pipefd_ptc[READ]);
   write(pipefd_ptc[WRITE],PING,sizeof(PING)+1);
-  close(pipefd_ptc[WRITE]);
+ //close(pipefd_ptc[WRITE]);
   printf("Parent sent a message to child!\n");
 }
 
@@ -64,15 +63,15 @@ void tell_child() {
  * 
  */
 void wait_child() {
-  /* TODO: Pipe handling missing*/
   char buf[99];
   ssize_t byte_count;
-  close(pipefd_ctp[WRITE]);                 //I am unsure how this affects the other process, that is potentially writing.
   byte_count = read(pipefd_ctp[READ], buf, sizeof(buf));
-  close(pipefd_ctp[READ]);
+  //close(pipefd_ctp[READ]);
   buf[byte_count] = '\0';
   printf("Parent received %s from the child!\n", buf);
 }
+
+
 
 int main(int argc, char* argv[]) {
   create_pipes();
@@ -86,10 +85,14 @@ int main(int argc, char* argv[]) {
   // Set a timer, the process will end after 10 seconds - sends an SIGALRM after the passage of this time.
   // The default disposition for SIGALRM is TERM(ination) ==> alarm kills our parent process
   // The OS will reap our zombie child (yeah admittdedly no clean solution)
+
   alarm(10);
+
   if (pid == 0) {
     // Parent process
-    // TODO: You might need pipe handling here
+    
+    close(pipefd_ctp[WRITE]);                
+    close(pipefd_ptc[READ]);
     while (1) {
       sleep(rand() % 2 + 1);
       tell_child();
@@ -98,12 +101,19 @@ int main(int argc, char* argv[]) {
   }
   else {
     // Child process
-    // TODO: You might need pipe handling here
+    
+    close(pipefd_ptc[WRITE]);                
+    close(pipefd_ctp[READ]);
     while (1) {
       sleep(rand() % 2 + 1);
       wait_parent();
       tell_parent();
     }
   }
+  
+  close(pipefd_ctp[WRITE]);                
+  close(pipefd_ptc[WRITE]);
+  close(pipefd_ctp[READ]);                
+  close(pipefd_ptc[READ]);
   return 0;
 }
